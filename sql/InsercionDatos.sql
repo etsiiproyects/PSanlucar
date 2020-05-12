41ea105f7b6e5bbefb18b6ad5bab42cf272affee
etsiiproyects <etsiiproyects@gmail.com>
2020-05-12 13:23:52 +0200

    procedures quitar

--- sql/InsercionDatos.sql
+++ sql/InsercionDatos.sql
@@ -1,11 +1,13 @@
 begin
 insertar_empleado('Admin1','123456As');
 insertar_empleado('Admin2','123456Sa');
+quitar_empleado(2);
 
 Insertar_Usuario('Pablo', 'Gonzalez Rodriguez', 'pablom@gmail.com' ,'AsDf1234','moncalvillo3');
 Insertar_Usuario('Jose Carlos', 'Morales Borreguero', 'josecb@gmail.com', 'Qwerty12', 'morales');
 Insertar_Usuario('Jose Calors', 'Romero Pozo', 'josecr@gmail.com', 'Qwerty12', 'romero');
 Insertar_Usuario('Fernando', 'Valdes Navarro', 'fernandov@gmail.com', 'Qwerty12', 'valdes');
+Quitar_Usuario('morales');
 
 insertar_inmueble('01.00','C/ San Juan de la Cruz',4, 'aislado');
 insertar_inmueble('02.1A','C/ Cristobal Colon',2, 'plurifamiliar');
@@ -17,6 +19,7 @@ insertar_inmueble('03.1B','C/ San Bartolome',1,'plurifamiliar');
 insertar_inmueble('03.1C','C/ San Bartolome',1,'plurifamiliar');
 insertar_inmueble('03.1D','C/ San Bartolome',2,'plurifamiliar');
 insertar_inmueble('04.1B','C/ Zambullon',2,'plurifamiliar');
+quitar_inmueble('03.1D');
 
 Insertar_oferta(600, sysdate,'01.00');
 Insertar_oferta(375, sysdate,'02.1A');
@@ -27,5 +30,6 @@ Insertar_oferta(350, sysdate,'03.1A');
 Insertar_oferta(350, sysdate,'03.1B');
 Insertar_oferta(350, sysdate,'03.1C');
 Insertar_oferta(375, sysdate,'04.1B');
+quitar_oferta(9);
 end;
 /
