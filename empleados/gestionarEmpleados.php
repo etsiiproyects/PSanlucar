7366e2eb45afb7585a5e4a44672b1b9cb8b1cb8a
Fernando <etsiiproyects@gmail.com>
2020-05-12 16:38:34 +0200

    a

--- empleados/gestionarEmpleados.php
+++ empleados/gestionarEmpleados.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 
 function consultarEmpleado($conexion,$usuario,$pass) {
- 	$consulta = "SELECT COUNT(*) FROM EMPLEADO WHERE NOMBRE=:usuario AND PASS=:pass";
+ 	$consulta = "SELECT COUNT(*)  FROM EMPLEADO WHERE NOMBRE=:nombre AND PASS=:pass";
 	$stmt = $conexion->prepare($consulta);
-	$stmt->bindParam(':usuario',$usuario);
+	$stmt->bindParam(':nombre',$usuario);
 	$stmt->bindParam(':pass',$pass);
 	$stmt->execute();
 	return $stmt->fetchColumn();
