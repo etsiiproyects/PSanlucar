c05d93d818efea387d462f09817b5e47a6f89cc1
etsiiproyects <etsiiproyects@gmail.com>
2020-05-27 19:11:56 +0200

    filtro

--- consulta_inmuebles.php
+++ consulta_inmuebles.php
@@ -30,6 +30,7 @@
 
 	$query = 'SELECT * FROM INMUEBLES';
 
+
 	// Se comprueba que el tamaño de página, página seleccionada y total de registros son conformes.
 	// En caso de que no, se asume el tamaño de página propuesto, pero desde la página 1
 	$total_registros = total_consulta($conexion, $query);
@@ -45,6 +46,22 @@
 	$_SESSION["paginacion"] = $paginacion;
 
 	$filas = consulta_paginada($conexion, $query, $pagina_seleccionada, $pag_tam);
+
+	$queryC= 'SELECT * FROM INMUEBLES WHERE ID_INMUEBLE NOT IN (SELECT ID_INMUEBLE FROM OFERTAS NATURAL JOIN DEMANDAS NATURAL JOIN CONTRATOS WHERE SYSDATE BETWEEN INICIOALQUILER AND FINALQUILER);'
+
+	$total_registrosC = total_consulta($conexion, $queryC);
+	$total_paginasC = (int)($total_registrosC / $pag_tamC);
+
+	if ($total_registrosC % $pag_tamC > 0)		$total_paginasC++;
+
+	if ($pagina_seleccionadaC > $total_paginasC)		$pagina_seleccionadaC = $total_paginasC;
+
+	// Generamos los valores de sesión para página e intervalo para volver a ella después de una operación
+	$paginacionC["PAG_NUM"] = $pagina_seleccionadaC;
+	$paginacionC["PAG_TAM"] = $pag_tamC;
+	$_SESSION["paginacionC"] = $paginacionC;
+
+	$filasC = consulta_paginadaC($conexion, $queryC, $pagina_seleccionadaC, $pag_tamC);
 
 	//$filas = consultarTodosInmuebles($conexion);
 	cerrarConexionBD($conexion);
@@ -88,6 +105,29 @@
 		</form>
 
 	</nav>
+
+	<nav class="pagC">
+		<div id="enlaces">
+			<?php
+				for( $paginaC = 1; $paginaC <= $total_paginasC; $paginaC++ )
+					if ( $paginaC == $pagina_seleccionadaC) { 	?>
+						<span class="current"><?php echo $paginaC; ?></span>
+			<?php }	else { ?>
+						<a href="consulta_inmuebles.php?PAG_NUM=<?php echo $paginaC; ?>&PAG_TAMC=<?php echo $pag_tamC; ?>"><?php echo $paginaC; ?></a>
+			<?php } ?>
+		</div>
+
+		<form method="get" action="consulta_inmuebles.php">
+			<input id="PAG_NUMC" name="PAG_NUMC" type="hidden" value="<?php echo $pagina_seleccionada?>"/>
+			Mostrando
+			<input id="PAG_TAMC" name="PAG_TAMC" type="number"
+				min="1" max="<?php echo $total_registrosC; ?>"
+				value="<?php echo $pag_tamC?>" autofocus="autofocus" />
+			entradas de <?php echo $total_registrosC?>
+			<input type="submit" value="Cambiar">
+		</form>
+
+	</nav>
 	<!-- <img class="iinmueble"  src="../images/inmueble.png" width="400px"> -->
 <section class="contenido">
 	<?php if(isset($_SESSION['loginEmpleado'])){ ?>
@@ -98,8 +138,9 @@
 
 
 	<h1>Lista de Inmuebles: </h1>
+	<button type="button" name="button" onclick="visibility();"> Mostras inmuebles libres</button>
 
-	<div class="inmuebles">
+	<div id="inmuebles" class="inmuebles">
 		<?php
 			foreach($filas as $fila) {
 		?>
@@ -142,6 +183,49 @@
 		<?php } ?>
 	</div>
 
+	<div id="freeInmuebles" class="inmueblesC">
+		<?php
+			foreach($filasC as $filaC) {
+		?>
+		<form method="post" action="controlador.php">
+		<div class="inmueble">
+			<div class="nameBx">
+				<img src="images/ritual.jpg" width="300px">
+			</div>
+			<div class="infoBx">
+				<input id="ID_INMUEBLE" name="ID_INMUEBLE" type="hidden" value="<?php echo $filaC["ID_INMUEBLE"]; ?>"/>
+				<h2>Inmueble: <b><?php echo $fila["ID_INMUEBLE"]; ?></b></h2>
+				<input id="DIRECCION" name="DIRECCION" type="hidden" value="<?php echo $filaC["DIRECCION"]; ?>"/>
+				<p>Direccion: <b><?php echo $fila["DIRECCION"]; ?></b></p>
+				<input id="HABITACIONES" name="HABITACIONES" type="hidden" value="<?php echo $filaC["HABITACIONES"]; ?>"/>
+				<p>Numero de habitaciones: <b><?php echo $filaC["HABITACIONES"]; ?></b></p>
+				<input id="TIPO" name="TIPO" type="hidden" value="<?php echo $filaC["TIPO"]; ?>"/>
+				<p>Tipo de inmueble: <b><?php echo $filaC["TIPO"]; ?></b></p>
+
+				<?php if(isset($_SESSION['loginEmpleado'])){ ?>
+
+					<?php if (isset($inmueble) and ($inmueble["ID_INMUEBLE"] == $filaC["ID_INMUEBLE"])) { ?>
+						<button id="grabar" name="grabar" type="submit" >
+							Guardar
+						</button>
+						<?php } else { ?>
+						<button id="editar" name="editar" type="submit" >
+							Modificar
+						</button>
+						<?php } ?>
+						<form method="post" action="controlador.php">
+							<input id="ID_INMUEBLE" name="ID_INMUEBLE" type="hidden" value="<?php echo $filaC["ID_INMUEBLE"]; ?>" />
+							<button id="borrar" name="borrar" type="submit">Borrar</button>
+						</form>
+
+					<?php } ?>
+					</form>
+			</div>
+		</div>
+		</form>
+		<?php } ?>
+	</div>
+
 
 </section>
 
@@ -149,6 +233,20 @@
 <?php
 	include_once("footer.php");
 ?>
+
+<script type="text/javascript">
+
+	let all=document.getElementById("inmuebles");
+	let free=document.getElementById("freeInmuebles");
+	 function visibility(){
+		 if(all.style.visibility=="visible"){
+			 all.style.visibility=="hidden";
+			 free.style.visibility=="visible";
+			 return;
+		 }
+	 }
+	funci
+</script>
 
 </body>
 </html>
