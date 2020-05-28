aed7c64f0a6b30bb8092419f840b7625bcdd9abd
etsiiproyects <etsiiproyects@gmail.com>
2020-05-28 23:30:09 +0200

    modifica

--- modificaContrato.php
+++ modificaContrato.php
@@ -28,7 +28,7 @@
 
 	<div class="iniciosesion">
 		<a href="index.php"><img class="img-registro" src="images/logo1.PNG" alt="Promociones Sanlúcar" /></a>
-		<form id="registro" method="get" action="accion_modificar_contrato.php">
+		<form id="registro" method="get" action="validacionModificaContrato.php">
 			<input type="text" name="oid" value=""<?php echo $contrato['OID_CONTRATO'] ?>"" hidden>
 			<label for="INICIOALQUILER">Inicio alquiler: </label>
 			<input class="input-group" id="INICIOALQUILER" name="INICIOALQUILER" type="date" />
