e19181cc2a99d93b8438eb39e069003c4dee830b
etsiiproyects <etsiiproyects@gmail.com>
2020-05-28 19:51:38 +0200

    a

--- modificaContrato.php
+++ modificaContrato.php
@@ -1,14 +1,14 @@
 <?php
 
 	session_start();
-	
+
 	if(isset($_REQUEST["OID_CONTRATO"])) {
 		$contrato["INICIOALQUILER"] = $_REQUEST["INICIOALQUILER"];
 		$contrato["FINALQUILER"] = $_REQUEST["FINALQUILER"];
 		$contrato["PAGOMENSUAL"] = $_REQUEST["PAGOMENSUAL"];
 		$contrato["NIF"] = $_REQUEST["NIF"];
 		$contrato["OID_CONTRATO"] = $_REQUEST["OID_CONTRATO"];
-		
+
 		$_SESSION["contrato"] = $contrato;
 	}
 	else header("Location: consulta_contratos.php");
@@ -25,14 +25,15 @@
 </head>
 
 <body>
-	
+
 	<div class="iniciosesion">
 		<a href="index.php"><img class="img-registro" src="images/logo1.PNG" alt="Promociones Sanlúcar" /></a>
 		<form id="registro" method="get" action="accion_modificar_contrato.php"></form>
 		<label for="INICIOALQUILER">Inicio alquiler: </label>
 		<input class="input-group" id="INICIOALQUILER" name="INICIOALQUILER" type="date" />
 		<p id="ia1" ><?php echo $contrato['INICIOALQUILER'] ?></p>
-		
+		<p id="ia2"></p>
+
 		<br />
 		<label for="FINALQUILER">Fin alquiler: </label>
 		<input class="input-group" id="FINALQUILER" name="FINALQUILER" type="text" value="<?php echo $contrato['FINALQUILER'] ?>" />
@@ -45,14 +46,15 @@
 		<br />
 		<input class="boton" type="submit" value="Confirmar cambios" />
 	</div>
-	
+
 	<script>
 		$(document).ready(function() {
 			let ia1 = document.getElementById("ia1").innerText.split("/");
 			let ia = document.getElementById("INICIOALQUILER");
 			let fecha="20" + ia1[2] + "-" + ia1[1] + "-" + ia1[0];
+			ia2.innetHTML=fecha;
 			ia.value = fecha;
 		})
 	</script>
-	
+
 </body>
