ec7263e0b4e4aad667e414811192210c63d96a82
Romero <etsiiproyects@gmail.com>
2020-05-28 19:48:55 +0200

    modifica

--- modificaContrato.php
+++ modificaContrato.php
@@ -27,9 +27,32 @@
 <body>
 	
 	<div class="iniciosesion">
+		<a href="index.php"><img class="img-registro" src="images/logo1.PNG" alt="Promociones Sanlúcar" /></a>
 		<form id="registro" method="get" action="accion_modificar_contrato.php"></form>
 		<label for="INICIOALQUILER">Inicio alquiler: </label>
-		<input class="input-group" id="INICIOALQUILER" name="INICIOALQUILER" type="text" value="<?php echo $contrato['INICIOALQUILER'] ?>" />
+		<input class="input-group" id="INICIOALQUILER" name="INICIOALQUILER" type="date" />
+		<p id="ia1" ><?php echo $contrato['INICIOALQUILER'] ?></p>
+		
+		<br />
+		<label for="FINALQUILER">Fin alquiler: </label>
+		<input class="input-group" id="FINALQUILER" name="FINALQUILER" type="text" value="<?php echo $contrato['FINALQUILER'] ?>" />
+		<br />
+		<label for="PAGOMENSUAL">Pago mensual: </label>
+		<input class="input-group" id="PAGOMENSUAL" name="PAGOMENSUAL" type="text" value="<?php echo $contrato['PAGOMENSUAL'] ?>" />
+		<br />
+		<label for="NIF">NIF: </label>
+		<input class="input-group" id="NIF" name="NIF" type="text" value="<?php echo $contrato['NIF'] ?>" />
+		<br />
+		<input class="boton" type="submit" value="Confirmar cambios" />
 	</div>
+	
+	<script>
+		$(document).ready(function() {
+			let ia1 = document.getElementById("ia1").innerText.split("/");
+			let ia = document.getElementById("INICIOALQUILER");
+			let fecha="20" + ia1[2] + "-" + ia1[1] + "-" + ia1[0];
+			ia.value = fecha;
+		})
+	</script>
 	
 </body>
