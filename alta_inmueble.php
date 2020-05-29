7c49c3f7d196b162e3213653d28150ad3c5ad610
etsiiproyects <etsiiproyects@gmail.com>
2020-05-29 23:57:59 +0200

    cftyvghbjnmkl

--- alta_inmueble.php
+++ alta_inmueble.php
@@ -8,7 +8,7 @@
         $nuevoInmueble=$_SESSION["inmueble"];
         $_SESSION["inmueble"] = null;
 		$_SESSION["errores"] = null;
-    }else Header("Location: registroInmueble.php");
+    }else Header("Location: formularioInmueble.php");
 
     $conexion=crearConexionBD();
 
@@ -16,7 +16,7 @@
         Header("Location: consulta_inmuebles.php");
     }else{
         $_SESSION["excepcion"] = "El inmueble ya existe en la base de datos.";
-        $_SESSION["destino"] = "registroInmueble.php";
+        $_SESSION["destino"] = "formularioInmueble.php";
         Header("Location: excepcion.php");
     }
     
