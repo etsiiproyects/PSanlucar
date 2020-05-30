7eae0a2e72c1f70ba31dad8307f046798c6baab8
etsiiproyects <etsiiproyects@gmail.com>
2020-05-30 13:06:50 +0200

    a

--- paginaprincipal.php
+++ paginaprincipal.php
@@ -7,14 +7,6 @@
 	require_once("gestionContratos.php");
 	require_once("paginacion_consulta.php");
 
-	if(!isset($_SESSION['login']) && !isset($_SESSION['loginEmpleado'])) {
-		header("Location: login.php");
-	} else {
-		if(isset($_SESSION["inmueble"])) {
-			$inmueble = $_SESSION["inmueble"];
-			unset($_SESSION["inmueble"]);
-		}
-  }
 
 ?>
 
@@ -25,27 +17,19 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Promociones Sanlucar: Lista de inmuebles</title>
-  <!-- <link rel="stylesheet" type="text/css" href="css/inmuebles.css">
-  <link rel="stylesheet" type="text/css" href="css/paginacion.css">
-  <link rel="stylesheet" type="text/css" href="css/menuNav.css">
-  <link rel="stylesheet" type="text/css" href="css/responsive.css">
-  <script src="js/inmuebles.js" type="text/javascript"></script>
-  <script src="js/responsive.js" type="text/javascript"></script>
-  <script src="js/toggle.js" type="text/javascript"></script> -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<script>
+  <script>
 		$(document).ready(function() {
 			var trigger =$('#nav ul li a'),
 				container=$('#content');
 
 			trigger.on('click', function() {
 				var $this=$(this) target=$this.data('target');
-			  container.load(target + '.php');
-
+			    container.load(target + '.php');
 				return false;
 			});
 		});
-	 </script>
+	</script>
 </head>
 
 <body>
