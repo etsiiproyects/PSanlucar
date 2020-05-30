ab34f32f5809544e24928f96e9f4c81b228a4d81
Romero <etsiiproyects@gmail.com>
2020-05-30 19:19:21 +0200

    borrar demanda

--- controladorDemanda.php
+++ controladorDemanda.php
@@ -2,12 +2,13 @@
 
 	session_start();
 	
-	if (isset($_REQUEST["demanda"])){
+	if (isset($_REQUEST["OID_DEMANDA"])){
 		$demanda["PRECIOMAX"] = $_REQUEST["PRECIOMAX"];
 		$demanda["FECHADEMANDA"] = $_REQUEST["FECHADEMANDA"];
 		$demanda["NUM_MASCOTA"] = $_REQUEST["NUM_MASCOTA"];
 		$demanda["NIF"] = $_REQUEST["NIF"];
 		$demanda["ID_INMUEBLE"] = $_REQUEST["ID_INMUEBLE"];
+		$demanda["OID_DEMANDA"] = $_REQUEST["OID_DEMANDA"];
 		
 		$_SESSION["demanda"] = $demanda;
 			
